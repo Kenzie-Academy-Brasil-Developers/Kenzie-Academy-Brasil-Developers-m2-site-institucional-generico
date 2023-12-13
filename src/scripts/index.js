1e31451e64993aff879e03b9207c4dff754dab64
JulyannaGama <julyannagama6@gmail.com>
2023-12-12 21:28:43 -0300

    Tarefa 4

---
+++ src/scripts/index.js
@@ -0,0 +1,17 @@
+function handleModalUser() {
+    const buttons = document.querySelectorAll('.button_modal');
+
+    const modalController = document.querySelector('#modal');
+    const close = document.querySelector('.modal_close')
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            modalController.showModal();
+        })
+    }
+    close.addEventListener('click', function () {
+        modalController.close();
+    })
+}
+
+handleModalUser()
